8dbc0af8d1daf0b29709807304bd89cf9c9bfa0b
Domenic Jernigan <jernigandomenic@gmail.com>
2026-05-03 22:19:09 -0700

    test deploy: add hero text

--- code/portfolio/src/app/page.tsx
+++ code/portfolio/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">        <h1 className="text-4xl text-white p-8">Hello from Dom!</h1>
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1 className="text-4xl text-white p-8">Hello from Dom!</h1>
         <Image
           className="dark:invert"
